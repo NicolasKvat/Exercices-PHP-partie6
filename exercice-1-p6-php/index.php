59330d498126030b4585ea724cb6a991b81b79e4
Nicolas Kvaternik <nicolaskvaternik@gmail.com>
2019-06-04 12:08:53 +0200

    first commit

---
+++ exercice-1-p6-php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Exercice 1 PHP</title>
+  </head>
+  <body>
+    <div class="container p-0">
+      <header>
+        <div class="row mb-4 p-4 bg-primary d-flex flex-column justify-content-center align-item-center">
+          <h1 class="text-white m-2 text-center">Exercice 1</h1>
+          <h4 class="text-white m-4 text-center">Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?lastname=Nemare&firstname=Jean</h4>
+        </div>
+      </header>
+      <p class="text-center">
+        <?php
+         echo 'bonjour ' . $_GET['firstname'] . ' ' . $_GET['lastname']; 
+        ?>
+      </p>
+    </div>
+  </body>
+</html>
